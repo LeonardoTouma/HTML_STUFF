12666aa0330489b4817907b554ef29ff9acc0a9f
LeonardoTouma <mr_milan1@hotmail.com>
2016-05-24 17:47:37 +0200

    Lab3, med sticky notes, hur man ska pina dem, hover och sätta de på sidan.

---
+++ Lab4/scripts/Jscript.js
@@ -0,0 +1,11 @@
+﻿$('img').each(function () {
+    var deg = $(this).data('rotate') || 0;
+    var rotate = 'rotate(' + $(this).data('rotate') + 'deg)';
+    $(this).css({
+        '-webkit-transform': rotate,
+        '-moz-transform': rotate,
+        '-o-transform': rotate,
+        '-ms-transform': rotate,
+        'transform': rotate
+    });
+});
