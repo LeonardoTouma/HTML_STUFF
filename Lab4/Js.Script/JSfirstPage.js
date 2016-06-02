d65c59518d8965eca99e6542f3ecd1954237ce54
LeonardoTouma <mr_milan1@hotmail.com>
2016-06-02 22:19:04 +0200

    Klar med Lab4, den blev inte den finaste men fick lärare mig otroligt mycket och det var kul att sitta med det, även fast att jag inte tog in tillräkligt med bootstrap.

---
+++ Lab4/Js.Script/JSfirstPage.js
@@ -0,0 +1,9 @@
+﻿
+function saveValue(target) {
+    var stepVal = document.getElementById(target).value;
+    alert("Nothing was found on your search: " + stepVal)
+}
+function saveValue(target) {
+    var stepVal = document.getElementById(target).value;
+    alert("We noted your email adress and will send you an form, where you can answear us back with your specifik needs. Double check so that your email is correct: " + stepVal)
+}
